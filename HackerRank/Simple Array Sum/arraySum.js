e0300cb4e2318b530eb32d9654c68b6b61ea24d9
Ujjwal Kumar <72997705+masteryodaa@users.noreply.github.com>
2021-10-04 23:12:25 +0530

    Simple Array Sum in JS.

---
+++ HackerRank/Simple Array Sum/arraySum.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+
+let sum = 0;
+function simpleArraySum(ar) {
+
+    // Write your code here
+  
+    
+    if(ar.length>0){
+        
+        for (let i = 0; i <= ar.length - 1; i++) {
+
+            if (ar[i] <= 1000) {
+
+                sum += ar[0];
+                ar = ar.slice(1);
+                return simpleArraySum(ar);
+                
+            }  else return console.log('elements should not be greater than 1000');
+        }
+        
+    } else return sum;
+
+}
+
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = simpleArraySum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
