1f6ff2d62c13fb4df5eb9cf5d5fa8ff94ca0e9d6
wilson furtado <wilsonfurtado2000@gmail.com>
2021-10-23 09:00:47 -0700

    simple array

---
+++ HackerRank/Simple Array Sum/wilsonfurtad2000_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int number_of_elements;
+    cin >> number_of_elements;
+    vector <int> array(number_of_elements);
+    int sum_of_array = 0;
+    
+    for(int i = 0; i < number_of_elements; i++){
+       cin >> array[i];
+       sum_of_array += array[i];
+    }
+    
+    cout << sum_of_array;
+    return 0;
+}
